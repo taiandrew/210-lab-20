09c06c29d5ab0ed1933ba7827af7baa3d5894ff3
taiandrew <31713490+taiandrew@users.noreply.github.com>
2025-10-06 11:48:39 -0700

    Random seed; 3rd instance to default constructor

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iomanip>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 const int SIZE = 3;
@@ -56,6 +58,8 @@ public:
 };
 
 int main() {
+
+    srand(time(0));
     cout << fixed << setprecision(2);
 
     //creating pointer to first chair object
@@ -73,12 +77,14 @@ int main() {
 
     //creating dynamic array of chair objects
     Chair *collection = new Chair[SIZE];
+    /*
     collection[0].setLegs(4);
     collection[0].setPrices(441.41, 552.52, 663.63);
     collection[1].setLegs(4);
     collection[1].setPrices(484.84, 959.59, 868.68);
     collection[2].setLegs(4);
     collection[2].setPrices(626.26, 515.15, 757.57);
+    */
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
     
